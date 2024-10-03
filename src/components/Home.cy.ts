a29b93022bed77dd919a763c6ab00c048f0812f5
Rambod Rahmani <rambodrahmani@aladia.io>
2024-10-03 15:02:48 +0200

    implement first e2e test sample

--- src/components/Home.cy.ts
+++ src/components/Home.cy.ts
@@ -6,7 +6,19 @@ describe("<Home />", () => {
   });
 });
 
-describe("Home Component", () => {
+describe("Home Component", function () {
+  it("renders the Home component with the correct message", () => {
+    cy.mount(Home, {
+      props: {
+        msg: "Title to be matched",
+      },
+    });
+
+    cy.get("h1").should("contain", "Title to be matched");
+  });
+});
+
+describe("Home Component", function () {
   it("renders the Home component with the correct message", () => {
     cy.mount(Home, {
       props: {
@@ -17,3 +29,11 @@ describe("Home Component", () => {
     cy.get("h1").should("contain", "Title to be matched (BUG)");
   });
 });
+
+// cy.visit from a component spec is not allowed
+
+// describe("Visit external page", function () {
+//   it("visits google homepage", () => {
+//     cy.visit("https://google.com");
+//   });
+// });
