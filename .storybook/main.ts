90048ad941aef311f55d306af169cdc9d670182a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-10-08 10:26:19 +0300

    chromatic config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
@@ -9,9 +10,12 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
+
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
   },
+
+  docs: {}
 };
 export default config;
